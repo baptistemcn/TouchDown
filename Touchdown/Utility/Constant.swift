7f0a7d6f9248d98c317b6359bd7bf08bd99f2ed6
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-27 19:23:59 +0200

    feat: Color Constant v1

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Baptiste on 27/05/2021.
+//
+
+import SwiftUI
+
+// API
+// COLOR
+let colorBackground: Color = Color("ColorBarkground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// DATA
+// FONT
+// IMAGE
+// LAYOUT
+// MISC
+// STRING
+// UX
