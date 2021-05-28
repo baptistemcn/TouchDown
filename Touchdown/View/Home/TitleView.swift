0811265ba2666d6c9e9a8555758aeca627c00f40
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-28 18:44:50 +0200

    feat: Title View

---
+++ Touchdown/View/Home/TitleView.swift
@@ -0,0 +1,36 @@
+//
+//  TitleView.swift
+//  Touchdown
+//
+//  Created by Baptiste on 28/05/2021.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    // MARK: - PROPERTIES
+    var title: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.title)
+                .fontWeight(.heavy)
+            
+            Spacer()
+        } //: HSTACK
+        .padding(.horizontal)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+// MARK: - PREVIEW
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView(title: "Helmet")
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
