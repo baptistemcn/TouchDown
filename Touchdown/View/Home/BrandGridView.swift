7a9e88fe4f3285c3ae0c3d58dbed809b8ec88bf2
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-28 19:26:59 +0200

    feat: Brand Item & Grid View

---
+++ Touchdown/View/Home/BrandGridView.swift
@@ -0,0 +1,30 @@
+//
+//  BrandGridView .swift
+//  Touchdown
+//
+//  Created by Baptiste on 28/05/2021.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridLayout, spacing: columnSpacing, content: {
+                ForEach(brands) { brand in
+                    BrandItemView(brand: brand)
+                }
+            }) //: GRID
+            .frame(height: 200)
+            .padding(15)
+        }) //: SCROLL
+    }
+}
+
+struct BrandGridView__Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
