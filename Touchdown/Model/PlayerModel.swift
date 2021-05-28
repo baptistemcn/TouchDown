8215c25e185ffc86b4929b728ddefe593e50fb6f
Baptiste Marcon <baptistemcn@icloud.com>
2021-05-28 12:01:47 +0200

    data: Player Model

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Baptiste on 28/05/2021.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
